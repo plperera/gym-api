6894d8e774fe419bb1d1338ab4d6f1b9c524102e
Pedro Pereira <plp.leoncio@gmail.com>
2023-05-23 15:44:57 -0300

    feat: https server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
+import * as fs from 'fs';
+import * as https from 'https';
 import app, { init } from "./app";
 
 const port = +process.env.PORT || 5000;
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.upsportbrasil.com.br/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/www.upsportbrasil.com.br/fullchain.pem', 'utf8');
+
+const credentials: https.ServerOptions = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
 
 init().then(() => {
   app.listen(port, () => {
